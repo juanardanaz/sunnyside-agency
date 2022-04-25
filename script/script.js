2f6110257428a32a071fa3bd49d3b063bda2ad10
Juan <juancruzardanaz@gmail.com>
2022-04-24 23:53:06 -0300

    burger menu and breakpoints added

---
+++ script/script.js
@@ -0,0 +1,13 @@
+// HAMBURGER MENU
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+});
+
+document.querySelectorAll('.nav_link').forEach(n => n.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
+}));
