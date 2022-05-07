9b30c7feb5e35a55a5a20f916e6eddbe8a4160b6
Juan <juancruzardanaz@gmail.com>
2022-05-07 20:18:56 -0300

    scroll-reveal added and text edit

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-// HAMBURGER MENU
+/*<!--=============== HAMBURGER MENU ===============-->*/
 const hamburger = document.getElementById('hamburger');
 const mobileMenu = document.getElementById('mobile_menu');
 
@@ -6,3 +6,18 @@ hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('hide');
     mobileMenu.classList.toggle('menu-mobile');
 });
+
+/*<!--=============== SCROLL REVEAL ===============-->*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 200,
+    reset: true,
+})
+
+sr.reveal(`.navbar`)
+sr.reveal(`.hero-text`, {delay: 400, origin: 'bottom'})
+sr.reveal(`.footer-menu`)
+sr.reveal(`.social-media`, {delay: 400, origin: 'bottom'})
+sr.reveal(`.footer-logo`)
